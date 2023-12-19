a622a290304bbd59f3c9f94bf2e98df9f8820525
Googolflex <golovinkirill@gmail37.com>
2023-12-19 16:14:22 +0700

    fixes

--- reminder/MainWindow.xaml.cs
+++ reminder/MainWindow.xaml.cs
@@ -1,21 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.Windows.Threading;
 using Window = System.Windows.Window;
-using System.Drawing;
 using Hardcodet.Wpf.TaskbarNotification;
 using System.ComponentModel;
 using System.IO;
@@ -23,9 +13,6 @@ using System.Xml.Serialization;
 
 namespace reminder
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         ObservableCollection<TaskItem> taskItems = new ObservableCollection<TaskItem>();
