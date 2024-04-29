f0d6ed8d2fc46be3cfb371ec2612282b1f579dfc
Googolflex <googolplexian00@gmail.com>
2024-04-29 18:09:06 +0700

    ...

---
+++ reminder/Models/GroupOfTasksItem.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace reminder.Models
+{
+    public class GroupOfTasksItem
+    {
+
+        private static int nextId = 0;
+
+        public int id;
+        public string name;
+        public string pathToTasks;
+        public SolidColorBrush groupColor;
+        public int Id
+        {
+            get { return nextId++; }
+        }
+
+        public string Name => name;
+        public string PathToTasks => pathToTasks;
+        public SolidColorBrush GroupColor => groupColor;
+    }
+}
