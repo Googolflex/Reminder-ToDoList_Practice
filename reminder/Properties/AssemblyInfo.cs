e70af89b0576b4c147fbf42ebff1cbca6d028b33
Googolflex <googolplexian00@gmail.com>
2025-06-12 19:03:31 +0700

    v1.0.4.1 Version number fix

--- reminder/Properties/AssemblyInfo.cs
+++ reminder/Properties/AssemblyInfo.cs
@@ -52,5 +52,5 @@ using System.Windows;
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.0.3.0")]
-[assembly: AssemblyFileVersion("1.0.3.0")]
+[assembly: AssemblyVersion("1.0.4.1")]
+[assembly: AssemblyFileVersion("1.0.4.1")]
